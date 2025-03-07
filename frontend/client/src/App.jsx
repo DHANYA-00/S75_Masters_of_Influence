cfcf1a6db48fb25a8fa9d3d0f500845cf133fa57
Dhanya Lakshmi s s <dhanyalakshmi.s.s.06@gmail.com>
2025-03-07 13:41:49 +0530

    created component

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import NewComponent from '../src/newComponent'
 
 function App() {
   return (
     <>
-    <div>
+    <div className='page'>
       <h1>Welcome to My ASAP Project</h1><hr></hr>
       <p>This project was chosen because its both creative and functional, allowing me to explore building a platform that showcases how people can transcend their initial professions and make significant impacts in various domains.</p>
     </div>
+    <NewComponent/>
     </>
   )
 }
